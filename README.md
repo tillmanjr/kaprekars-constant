3a530c031bc25435acaf2d56b10661461950aeeb
tillmanjr <tillmanjr@gmail.com>
2025-03-21 17:03:42 -0700

    translate to TypeScript

--- README.md
+++ README.md
@@ -1,17 +1,45 @@
 # kaprekars-constant  
 Derives Kaprekar's Constant from any 4 digit integer  
   
+Originally written in JavaScript which was then translated to TypeScript.  
+  
 ## Kaprekar's constant  
 Named after the Indian mathematician D.R. Kaprekar.  
    
 The number 6174, also known as Kaprekar's constant, is a fascinating number that always arises when repeatedly applying a specific mathematical operation to any four-digit number with at least two distinct digits.   
   
 ## To run it:  
+### JavaScript  
 Clone this repo or simply copy the JavaScript file locally.  
   
-Edit the test values array to include any integers desired  
-`    const testValues = [1205, 6174, 1234, 6598, 5432, 4321, 8835]`  
+Change to appropriate subdirectory  
+`cd javascript`   
+   
+In `KaprekarsConstant.js` edit the test values array to include any integers desired  
+`const testValues = [1205, 6174, 1234, 6598, 5432, 4321, 8835]`  
   
 Save the file     
   
+Run  
 `node KaprekarsConstant.js`  
+  
+### TypeScript  
+Clone this repo or simply copy the TypeScript file locally.  
+  
+Change to appropriate subdirectory  
+`cd typescript`   
+   
+In `KaprekarsConstant.ts` edit the test values array to include any integers desired  
+`const testValues:number[] = [1205, 6174, 1234, 6598, 5432, 4321, 8835]`  
+  
+Save the file     
+  
+Compile to JavaScript (or run directly with node if you have TypeScript support enabled for node) 
+`tsc --lib es2019,dom KaprekarsConstant.ts`  (see __TypeScript --lib parameters__ below)
+
+Run  
+`node KaprekarsConstant.js`  
+
+#### TypeScript --lib parameters  
+es2019 - JavaScript version emitted by compiler. Required here for use of `<array>.flat()`  
+dom - include DOM library. Required here for use of `console.log`  
---
+++ typescript/KaprekarsConstant.ts
@@ -0,0 +1,193 @@
+// Kaprekar's constant
+// Named after the Indian mathematician D.R. Kaprekar.
+// 
+// The number 6174, also known as Kaprekar's constant,
+//   is a fascinating number that arises when applying
+//   a specific mathematical operation to any four-digit 
+//   number with at least two distinct digits, 
+//   repeatedly leading to 6174.
+// Starting with the digits of any 4 digit number 
+//   1. create two new numbers:
+//   2. from the digits sorted in descending order of value, ref DESC
+//   3. from the digits sorted in descending order of value, ref ASC
+//   4. Subtract ASC from DESC to generate a new 4 digit number 
+//   5. Repeat 1-4 until the digits of DESC, ASC, and the new number are the same
+
+/** @type {number}  Kapreskar's Constant */
+const KaprekarsConstantValue:number=6174;
+
+
+/** @type {string} KaprekarsConstantValue as a string */
+const KaprekarsConstantString:string = KaprekarsConstantValue.toString();
+
+
+type OrdinalDifferenceResultType = {
+    value: number,
+    ascValue: number,
+    descValue: number,
+    difference: number
+};
+
+/**
+ *  For the digits of the passed value calculate the difference of
+ *    the digits sorted in descending order and 
+ *    the digits sorted in ascending order
+ * Returns an object 
+ *  {
+ *      value,       // original value
+ *      ascValue,    // orginal value with the digits sorted ascending 
+ *      descValue,   // orginal value with the digits sorted descending
+ *      difference   // result of descValue - ascValue
+ *  }
+ * @param {Number} value: integer where >= 1000 && <= 9999
+ * @return {object}{
+ *    value,
+ *    ascValue,
+ *    descValue,
+ *    difference
+ * }  
+ */
+function calculateOrdinalDifference(value:number):OrdinalDifferenceResultType {
+    if (value < 1000 || value > 9999) {
+        throw new Error('Value must be a four-digit number');
+    }
+    
+    const digits:number[] = value
+        .toString()
+        .split('')
+        .map(Number);
+    const asc:string = digits
+        .slice()
+        .sort((a:number, b:number) => a - b)
+        .join('');
+    const desc:string = digits
+        .slice()
+        .sort((a:number, b:number) => b - a)
+        .join('');
+
+    const ascValue:number = parseInt(asc, 10);
+    const descValue:number = parseInt(desc, 10);
+    const difference:number = descValue - ascValue;
+    
+    return {
+        value,
+        ascValue,
+        descValue,
+        difference
+    };
+}
+
+/**
+ * Converts an integer to a string.
+ * Return value will be left padded with '0' 
+ *   to ensure minimum of 4 char length 
+ *
+ * @param {Number} value: integer
+ * @return {string} .length >= 4 
+ */
+function ensure4DigitString(value:number):string {
+    let valueStr:string = value.toString();
+    while (valueStr.length < 4) {
+        valueStr = '0' + valueStr
+    }
+    return valueStr;
+}
+
+/**
+ * For one Kapersky Operation return the individual operations
+ *   as an array of formatted strings 
+ *
+ * @param {OrdinalDifferenceResultType} param0
+ * @return {string[]} 
+ */
+function oneResult({
+    value,
+    descValue,
+    ascValue,
+    difference
+}: OrdinalDifferenceResultType): string[] {
+    return [
+        `${value}`,
+        `       ${ensure4DigitString(descValue)}`,
+        `     - ${ensure4DigitString(ascValue)}`,
+        `     = ${ensure4DigitString(difference)}`
+    ]
+}
+
+/**
+ * Determine whether the individual digits of two integers are equivalent.
+ *   e.g.  lhs   rhs    returns
+ *         1234  4321   true
+ *         1234  1233   false
+ *
+ * @param {Number} lhs: integer
+ * @param {Number} rhs: integer
+ * @return {Boolean} 
+ */
+function haveSameDigits (lhs:number, rhs:number): boolean {
+    const lhsDigits:string = lhs
+        .toString()
+        .split('')
+        .map(Number)
+        .slice()
+        .sort((a, b) => a - b)
+        .join('')
+
+    const rhsDigits:string = rhs
+        .toString()
+        .split('')
+        .map(Number)
+        .slice()
+        .sort((a, b) => a - b)
+        .join('')
+
+    return lhsDigits === rhsDigits  // use compare instead?
+}
+
+/**
+ * Starting with startValue repeat Operation until Kapersky's constant is resolved.
+ * returns an array of strings containing details of each Operation in order of execution
+ *
+ * @param {Number} startValue: integer
+ * @return {string[]} 
+ */
+function seekKaprekarsConstant(startValue: number) {
+    let value: number = startValue
+    let output: string[] = ['   Begin   ']
+    let found: boolean = false
+    let safety: number = 30
+    while (!found) {
+        if (safety < 1) {
+            output.push('Seek failed to terminate')
+            break
+        }
+        safety = safety - 1
+
+        const result: OrdinalDifferenceResultType = calculateOrdinalDifference(value);
+        const oneOutput: string[] = oneResult(result)
+        output.push(...oneOutput)
+
+        found = haveSameDigits(value, result.difference)
+        value = result.difference;  
+    }
+    output.push('___________', '')
+    return output
+}
+
+/**
+ * Print line to console
+ *
+ * @param {string} line
+ */
+const printLine = (line:string) => console.log(line)
+
+function go(values:number[] = []) {
+    values
+        .map(seekKaprekarsConstant)
+        .flat()
+        .forEach(printLine)
+}
+
+
+const testValues:number[] = [1205, 6174, 1234, 6598, 5432, 4321, 8835]
+go(testValues)
