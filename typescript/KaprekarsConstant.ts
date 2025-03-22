e8b6ce5e60a19191574c043cd0e27dacead78e8a
Tillman Dickson, Jr. <tillmanjr@gmail.com>
2025-03-21 17:27:05 -0700

    Update KaprekarsConstant.ts
    fix commit weirdness

--- typescript/KaprekarsConstant.ts
+++ typescript/KaprekarsConstant.ts
@@ -1,4 +1,5 @@
 // Kaprekar's constant
+
 // Named after the Indian mathematician D.R. Kaprekar.
 // 
 // The number 6174, also known as Kaprekar's constant,
@@ -12,81 +13,95 @@
 //   3. from the digits sorted in descending order of value, ref ASC
 //   4. Subtract ASC from DESC to generate a new 4 digit number 
 //   5. Repeat 1-4 until the digits of DESC, ASC, and the new number are the same
+
 /** @type {number}  Kapreskar's Constant */
-var KaprekarsConstantValue = 6174;
+const KaprekarsConstantValue:number=6174;
+
 /** @type {string} KaprekarsConstantValue as a string */
-var KaprekarsConstantString = KaprekarsConstantValue.toString();
+const KaprekarsConstantString:string = KaprekarsConstantValue.toString();
+
+type OrdinalDifferenceResultType = {
+    value: number, // original value
+    ascValue: number, // orginal value with the digits sorted ascending
+    descValue: number, // orginal value with the digits sorted descending
+    difference: number  // result of descValue - ascValue
+};
+
 /**
  *  For the digits of the passed value calculate the difference of
- *    the digits sorted in descending order and
+ *    the digits sorted in descending order and 
  *    the digits sorted in ascending order
- * Returns an object
- *  {
- *      value,       // original value
- *      ascValue,    // orginal value with the digits sorted ascending
- *      descValue,   // orginal value with the digits sorted descending
- *      difference   // result of descValue - ascValue
- *  }
+ * Returns OrdinalDifferenceResultType 
  * @param {number} value: integer where >= 1000 && <= 9999
  * @return {OrdinalDifferenceResultType}
  */
-function calculateOrdinalDifference(value) {
+function calculateOrdinalDifference(value:number):OrdinalDifferenceResultType {
     if (value < 1000 || value > 9999) {
         throw new Error('Value must be a four-digit number');
     }
-    var digits = value
+    
+    const digits:number[] = value
         .toString()
         .split('')
         .map(Number);
-    var asc = digits
+    const asc:string = digits
         .slice()
-        .sort(function (a, b) { return a - b; })
+        .sort((a:number, b:number) => a - b)
         .join('');
-    var desc = digits
+    const desc:string = digits
         .slice()
-        .sort(function (a, b) { return b - a; })
+        .sort((a:number, b:number) => b - a)
         .join('');
-    var ascValue = parseInt(asc, 10);
-    var descValue = parseInt(desc, 10);
-    var difference = descValue - ascValue;
+
+    const ascValue:number = parseInt(asc, 10);
+    const descValue:number = parseInt(desc, 10);
+    const difference:number = descValue - ascValue;
+    
     return {
-        value: value,
-        ascValue: ascValue,
-        descValue: descValue,
-        difference: difference
+        value,
+        ascValue,
+        descValue,
+        difference
     };
 }
+
 /**
  * Converts an integer to a string.
- * Return value will be left padded with '0'
- *   to ensure minimum of 4 char length
+ * Return value will be left padded with '0' 
+ *   to ensure minimum of 4 char length 
  *
  * @param {number} value: integer
- * @return {string} .length >= 4
+ * @return {string} .length >= 4 
  */
-function ensure4DigitString(value) {
-    var valueStr = value.toString();
+function ensure4DigitString(value:number):string {
+    let valueStr:string = value.toString();
     while (valueStr.length < 4) {
-        valueStr = '0' + valueStr;
+        valueStr = '0' + valueStr
     }
     return valueStr;
 }
+
 /**
  * For one Kapersky Operation return the individual operations
- *   as an array of formatted strings
+ *   as an array of formatted strings 
  *
  * @param {OrdinalDifferenceResultType} param0
- * @return {string[]}
+ * @return {string[]} 
  */
-function oneResult(_a) {
-    var value = _a.value, descValue = _a.descValue, ascValue = _a.ascValue, difference = _a.difference;
+function oneResult({
+    value,
+    descValue,
+    ascValue,
+    difference
+}: OrdinalDifferenceResultType): string[] {
     return [
-        "".concat(value),
-        "       ".concat(ensure4DigitString(descValue)),
-        "     - ".concat(ensure4DigitString(ascValue)),
-        "     = ".concat(ensure4DigitString(difference))
-    ];
+        `${value}`,
+        `       ${ensure4DigitString(descValue)}`,
+        `     - ${ensure4DigitString(ascValue)}`,
+        `     = ${ensure4DigitString(difference)}`
+    ]
 }
+
 /**
  * Determine whether the individual digits of two integers are equivalent.
  *   e.g.  lhs   rhs    returns
@@ -95,64 +110,72 @@ function oneResult(_a) {
  *
  * @param {number} lhs: integer
  * @param {number} rhs: integer
- * @return {boolean}
+ * @return {boolean} 
  */
-function haveSameDigits(lhs, rhs) {
-    var lhsDigits = lhs
+function haveSameDigits (lhs:number, rhs:number): boolean {
+    const lhsDigits:string = lhs
         .toString()
         .split('')
         .map(Number)
         .slice()
-        .sort(function (a, b) { return a - b; })
-        .join('');
-    var rhsDigits = rhs
+        .sort((a, b) => a - b)
+        .join('')
+
+    const rhsDigits:string = rhs
         .toString()
         .split('')
         .map(Number)
         .slice()
-        .sort(function (a, b) { return a - b; })
-        .join('');
-    return lhsDigits === rhsDigits; // use compare instead?
+        .sort((a, b) => a - b)
+        .join('')
+
+    return lhsDigits === rhsDigits  // use compare instead?
 }
+
 /**
  * Starting with startValue repeat Operation until Kapersky's constant is resolved.
  * returns an array of strings containing details of each Operation in order of execution
  *
  * @param {number} startValue: integer
- * @return {string[]}
+ * @return {string[]} 
  */
-function seekKaprekarsConstant(startValue) {
-    var value = startValue;
-    var output = ['   Begin   '];
-    var found = false;
-    var safety = 30;
+function seekKaprekarsConstant(startValue: number) {
+    let value: number = startValue
+    let output: string[] = ['   Begin   ']
+    let found: boolean = false
+    let safety: number = 30
     while (!found) {
         if (safety < 1) {
-            output.push('Seek failed to terminate');
-            break;
+            output.push('Seek failed to terminate')
+            break
         }
-        safety = safety - 1;
-        var result = calculateOrdinalDifference(value);
-        var oneOutput = oneResult(result);
-        output.push.apply(output, oneOutput);
-        found = haveSameDigits(value, result.difference);
-        value = result.difference;
+        safety = safety - 1
+
+        const result: OrdinalDifferenceResultType = calculateOrdinalDifference(value);
+        const oneOutput: string[] = oneResult(result)
+        output.push(...oneOutput)
+
+        found = haveSameDigits(value, result.difference)
+        value = result.difference;  
     }
-    output.push('___________', '');
-    return output;
+    output.push('___________', '')
+    return output
 }
+
 /**
  * Print line to console
  *
  * @param {string} line
  */
-var printLine = function (line) { return console.log(line); };
-function go(values) {
-    if (values === void 0) { values = []; }
+const printLine = (line:string) => console.log(line)
+
+function go(values:number[] = []) {
     values
         .map(seekKaprekarsConstant)
         .flat()
-        .forEach(printLine);
+        .forEach(printLine)
 }
-var testValues = [1205, 6174, 1234, 6598, 5432, 4321, 8835];
-go(testValues);
+
+const testValues:number[] = [1205, 6174, 1234, 6598, 5432, 4321, 8835]
+
+go(testValues)
